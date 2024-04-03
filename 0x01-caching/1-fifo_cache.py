9f2391774c444e3fee14e84f7b035b278fc0b5b7
dimka90 <dimkayilrit@gmail.com>
2024-04-03 11:37:28 +0100

    created a FIFO cache system that remove the first item inserted into the dictionary first

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+"""
+from collections import OrderedDict
+BaseCache = __import__("0-basic_cache").BasicCache
+
+
+class FIFOCache(BaseCache):
+    """
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key: str, item: str) -> None:
+        """
+        Add an item in the cache
+
+        Arguments:
+                key(str): Key pair of the dictionary
+                item(str): Value to store to a pair of key
+        Return:
+                None or the value of the key obtain
+        """
+        if key is None or item is None:
+            return
+        len_cache = len(self.cache_data)
+        if len_cache < self.MAX_ITEMS:
+            self.cache_data[key] = item
+        else:
+            self.cache_data = OrderedDict(self.cache_data)
+            removed_key, removed_value = self.cache_data.popitem(last=False)
+            print("Discard: ", removed_key)
+            self.cache_data[key] = item
