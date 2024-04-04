62ec5c530e7d1c7986a372b15528b9bfb17c2db2
dimka90 <dimkayilrit@gmail.com>
2024-04-04 05:01:09 +0100

    created an algorthm that works on the principlr of least recently used

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+FIFOCache Module
+"""
+from collections import OrderedDict
+BaseCache = __import__("0-basic_cache").BasicCache
+
+
+class LRUCache(BaseCache):
+    """
+    A Subclass of BasicCache
+    """
+
+    def __init__(self):
+        """
+            self: Object to be created
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key: str, item: str) -> None:
+        """
+        Add an item in the cache
+
+        Arguments:
+                key(str): Key pair of the dictionary
+                item(str): Value to store to a pair of key
+        Return:
+                None or the value of the key obtain
+        """
+        if key is None or item is None:
+            return
+        len_cache = len(self.cache_data)
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+        if len_cache < BaseCache.MAX_ITEMS:
+            self.cache_data[key] = item
+        else:
+            removed_key, _ = self.cache_data.popitem(last=False)
+            print("DISCARD: {:s}".format(removed_key))
+            self.cache_data[key] = item
+
+    def get(self, key: str) -> str:
+        """
+        Get an item by key
+        Arguments:
+                key(str): Key pair of the dictionary
+        Return:
+               Stored value of the or None
+        """
+        return self.cache_data.get(key, None)
